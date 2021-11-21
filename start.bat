e365980223a86016866d6a62c65b7e8f7bf8cd6d
Skull Rain <94234562+SkullRa1n@users.noreply.github.com>
2021-11-21 18:46:37 +0000

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-echo "# SkullBot" >> README.md
-git init
-git add *
-git commit -m "Primeiro Update"
-git branch -M main
-git remote add origin https://github.com/SkullRa1n/SkullBot.git
-git push -u origin main
